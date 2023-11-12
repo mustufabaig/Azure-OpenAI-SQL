f6224d18ff931d1ef5edf9721f700c4b9b2ec066
mustufabaig <mustufa.baig@gmail.com>
2023-11-12 11:42:42 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,29 +8,19 @@ from prompts.prompts import SYSTEM_MESSAGE
 from azure_openai import get_completion_from_messages
 import json
 
+username = st.secrets["username"]
+password = st.secrets["password"]
+warehouse = st.secrets["warehouse"]
+role = st.secrets["role"]
+snowflake_account = st.secrets["account"]
+database = st.secrets["database"]
+schema = st.secrets["schema"]
+snowflake_url = f"snowflake://{username}:{password}@{snowflake_account}/{database}/{schema}?warehouse={warehouse}&role={role}"
+        
 def query_database(query):
     """ Run SQL query and return results in a dataframe """
     return pd.read_sql_query(query, conn)
 
 # Create or connect to SQLite database
-conn = sql_db.create_connection()
+conn = sql_db.create_connection(snowflake_url)
 
-# Schema Representation for finances table
-schemas = sql_db.get_schema_representation()
-print(schemas['finances'])
-
-# Format the system message with the schema
-formatted_system_message = SYSTEM_MESSAGE.format(schema=schemas['finances'])
-
-# Generate the SQL query from the user message
-user_message = "Show me all expenses greater than 1000"
-
-# Use GPT-4 to generate the SQL query
-response = get_completion_from_messages(formatted_system_message, user_message)
-json_response = json.loads(response)
-query = json_response['query']
-print(query)
-
-# Run the SQL query
-sql_results = query_database(query)
-print(sql_results)
